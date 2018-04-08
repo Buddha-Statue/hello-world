e7b77811b812d36fae22c60c00c8130593f1d399
William Cole_Baker <cole.power@hotmail.com>
2018-04-08 02:32:30 +0100

    Commiting basic structure and .gitignore
    Not a commit of noteriaty

---
+++ src/test/java/zephyr/UserTest.java
@@ -0,0 +1,15 @@
+package zephyr;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserTest {
+
+	@Test
+	public void createUserWithFadaSuccess() {
+		User testUser = new User("Sinéad");
+		fail("Not yet implemented");
+	}
+
+}
